718b177ec11d3267b3016ee3f450e23dc5d94099
fw <fw@123.com>
2022-05-19 11:22:05 +0800

    master second commit

--- src/main/java/com/zgsjwzdx/GitTest.java
+++ src/main/java/com/zgsjwzdx/GitTest.java
@@ -6,6 +6,7 @@ public class GitTest {
         System.out.println("hello git2!!!");
         System.out.println("hello git3!!!");
         System.out.println("hello git4 hot-fix!!!");
+        System.out.println("hello git5 master!!!");
 
     }
 }
